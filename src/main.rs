6e439c679dc77f65f5757cb756a0ff3292569fd5
tyz <tyz@1token.trade>
2021-07-30 20:58:21 +0800

    auto update

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,35 @@
 fn main() {
-	let light = TrafficLight::Red;
-	println!("light is {}", light.time());
-	// println!("plus  {}", plus_one(3));
-	let five = Some(5);
-
-	// let six = five.map(|i| i + 1);
-	// println!("six {:?}", six)
-
-	let number_list = vec![1,2,3,2];
-	let res = largest(&number_list);
-
-	println!("largest {}", res)
-}
-
-enum TrafficLight {
-	Red,
-	Green,
-	Yellow,
-}
-
-impl TrafficLight {
-	fn time(&self) -> u8 {
-		60
-	}
-}
-
-fn plus_one(x: Option<i32>) -> Option<i32> {
-	match x {
-		None => None,
-		Some(i) => Some(i + 1),
-	}
-}
-
-fn largest<T : PartialOrd>(list: &[T]) -> &T {
-	let mut lag = &list[0];
-	for item in list {
-		if item > lag {
-			lag = item
+	let arr: [u32; 4] = [5, 6, 7, 9];
+	let t = summary(&arr);
+	match t {
+		None => {
+			println!("none")
+		}
+		Some(t) => {
+			println!("some {}", t)
 		}
 	}
-	return lag;
+
+	let arr: [u32; 4] = [2100000000, 2100000000, 2100000000, 2100000000];
+	let t = summary(&arr);
+	match t {
+		None => {
+			println!("none, overflow!!")
+		}
+		Some(t) => {
+			println!("some {}", t)
+		}
+	}
 }
 
+fn summary(x: &[u32]) -> Option<u32> {
+	let mut t: u32 = 0;
+	for i in 0..x.len() {
+		match t.checked_add(x[i]) {
+			Some(v) => t = v,
+			None => return None,
+		}
+	}
+
+	return Some(t);
+}
